2d266e9b45d5bbd61644c8022fba2628699b1878
Sidpatchy <unconfigured@null.spigotmc.org>
2020-07-03 22:02:38 -0500

    v0.1-a7
    Fixed an issue that caused the library to ocassionally use "True" as the bot name. This probably isn't the best fix possible but it seems to work pretty well.

--- sLOUT.py
+++ sLOUT.py
@@ -7,7 +7,7 @@ import platform
 import glob
 
 # writeFile() writes a string to the specified file
-# Usage: 
+# Usage:
 #   file: string, filename
 #   string: string to be written to file.
 #   time: Boolean, whether or not the time should be logged.
@@ -20,7 +20,7 @@ def writeFile(file, string, time=False):
     f.close()
 
 # readFile() reads a file
-# Usage: 
+# Usage:
 #   file: string, filename
 def readFile(file):
     f = open(file, 'r')
@@ -64,11 +64,17 @@ def listBots():
 #   processName: string, the name of the command/process that was run
 #   botName: string, the name of the bot. Can be suplemented by placing the name you would like to use in a file named bot.txt
 #   startup: boolean, when true, it will print done loading and the time. If you are using bot.txt insert lout.readFile('bot.txt') as the third parameter or just insert the bot name as a string.
-def log(startTime, processName, botName=True, startup=False):
+botName = None
+def log(startTime, processName, name=True, startup=False):
+    global botName              # There is no reason for this to be like this. However, if it isn't a global variable, the automated selection system breaks
     if startup == True:
         # read the botname from the config
-        if botName == True:
-            botName == readConfig('*.bot', 'botName')                           # This is very dangerous...
+        if name == True:
+            botName == readConfig('*.bot', 'botName')                           # This is potentially dangerous... I recommend switching this to the config file of the bot you will be using for now
+        else:
+            botName = 'ERROR'
+            writeFile('sLOUT-error.txt', 'Bot name was unable to be read from file', True)
+            print('Please check sLOUT-error.txt for more details.')
 
         # Console Stuff
         print('--------------{}---------------'.format(botName))                # Divder in the console
@@ -85,8 +91,12 @@ def log(startTime, processName, botName=True, startup=False):
 
     elif startup == False:
         # read the botname from the config
-        if botName == True:
-            botName == readConfig('*.bot', 'botName')                           # This is very dangerous... I hope no one who uses this has any files that end in .bot
+        if name == True:
+            botName == readConfig('*.bot', 'botName')                           # This is potentially dangerous... I hope no one who uses this has any files that end in .bot
+        else:
+            botName = 'ERROR'
+            writeFile('sLOUT-error.txt', 'Bot name was unable to be read from file', True)
+            print('Please check sLOUT-error.txt for more details.')
 
         # Console Stuff
         print('--------------{}---------------'.format(botName))                # Divder in the console
@@ -116,3 +126,7 @@ def readConfig(file, parameter):
             parameter = parameter.replace('\n', '')
             parameter = parameter.replace(' ', '')
             return parameter
+
+# Simple method to get time that requires less typing than DT.datetime.now()
+def time():
+    return DT.datetime.now()
